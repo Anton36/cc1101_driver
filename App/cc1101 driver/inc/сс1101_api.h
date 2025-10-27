717b8fa79ddbc25e9a2aeb3fee077d178014da6d
Anton Mosolov <mosolov2000@gmail.com>
2025-10-27 13:56:32 +0300

    minor change api file

--- App/cc1101 driver/inc/сс1101_api.h
+++ App/cc1101 driver/inc/сс1101_api.h
@@ -2,36 +2,24 @@
 #ifndef __CC1101_API_H__
 #define __CC1101_API_H__
 
-
-#include <stdint.h>
 #include <stddef.h>
-
-
-
-
-
-
-
-
-
-
-
-
-
+#include <stdint.h>
 
 enum {
-    CC1101_OK = 0,
-    CC1101_TIMEOUT = -1,
-    CC1101_EINVAL = -2,
-    CC1101_EIO = -3,
+  CC1101_OK = 0,
+  CC1101_TIMEOUT = -1,
+  CC1101_EINVAL = -2,
+  CC1101_EIO = -3,
 
 };
 
 typedef struct {
-  void (*cs_set_level)(void *user,uint8_t level);
-  int (*spi_tx)(void *user,const uint8_t *address,uint16_t len,uint32_t timeout_ms);
-  int (*spi_rx)(void *user, uint8_t *buffer,uint16_t len,uint32_t timeout_ms);
-  int (*spi_txrx)(void *user,const uint8_t *address, uint8_t *buffer,uint16_t len,uint32_t timeout_ms);
+  void (*cs_set_level)(void *user, uint8_t level);
+  int (*spi_tx)(void *user, const uint8_t *address, uint16_t len,
+                uint32_t timeout_ms);
+  int (*spi_rx)(void *user, uint8_t *buffer, uint16_t len, uint32_t timeout_ms);
+  int (*spi_txrx)(void *user, const uint8_t *address, uint8_t *buffer,
+                  uint16_t len, uint32_t timeout_ms);
   void (*delay_us)(void *user, uint32_t us);
 
   void *user;
@@ -42,6 +30,5 @@ int cc1101_driver_init(cc1101_fun_table *fun_table);
 void cc1101_reset();
 bool cc1101_check();
 int load_config(uint8_t config);
-
 
 #endif
